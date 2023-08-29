dc774481bb9ff6f18fb3d970ba85362741d4d025
Caio Henrique <caiohenriquessiqueira.caike@gmail.com>
2023-08-29 10:10:38 -0300

    Adicionando Funcionalidade de Filtro

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,8 @@
 const pokemonList = document.querySelector('#pokemonList')
 const loadMoreButton = document.querySelector('#loadMoreButton')
-const limit = 5
+const limit = 100
 let offset = 0
+const textInput = document.querySelector('#search')
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = [])=> {
@@ -19,9 +20,28 @@ function loadPokemonItens(offset, limit) {
     })
 }
 
+function filterPokemons(searchTerm) {
+    const pokemons = pokemonList.querySelectorAll('.pokemon');
+    
+    pokemons.forEach(pokemon => {
+        const name = pokemon.querySelector('.name').textContent.toLowerCase();
+        
+        if (name.includes(searchTerm)) {
+            pokemon.style.display = 'block';
+        } else {
+            pokemon.style.display = 'none';
+        }
+    });
+}
+
 loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     loadPokemonItens(offset, limit)
 })
+
+textInput.addEventListener('input', () => {
+    const searchTerm = textInput.value.toLowerCase();
+    filterPokemons(searchTerm);
+});
